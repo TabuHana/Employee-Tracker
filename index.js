3863b9f9bcccab6d72d0ed68f9ba2be30aac6e18
Tabu Hana <taboodog@hotmail.com>
2022-04-13 10:40:42 -0700

    fixed code to run; added view departments

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 const mysql = require('mysql2')
 const inquirer = require('inquirer')
 
-const db = mysql.createConnection('root:300Monkeysonfire!!!@localhost/employeeManager_db')
+const db = mysql.createConnection('mysql://root:300Monkeysonfire!!!@localhost/employeeManager_db')
 
 // Need Type, name, message. Prompt for event
 const homeScreen = () => {
   inquirer.prompt([{
     type: 'list',
-    name: 'home',
+    name: 'homeSelect',
     message: 'Select an option',
     choices: ['Add Department', 'Add Role', 'Add Employee', 'View Departments', 'View Roles', 'View Employees', 'Update Employee Role']
   }])
-     .then(home => {
-       console.log(home)
-    //   switch (home.home) {
-    //     case 'Add Department':
-    //       addDepartment()
-    //       break
+    .then(home => {
+      switch (home.homeSelect) {
+        case 'Add Department':
+          addDepartment()
+          break
 
-        // case 'Add Role':
-        //   addRole()
-        //   break
+        case 'Add Role':
+          addRole()
+          break
 
-        // case 'Add Employee':
-        //   addEmployee()
-        //   break
+        case 'Add Employee':
+          addEmployee()
+          break
 
         // case 'View Departments':
         //   viewDepartments()
@@ -45,106 +44,114 @@ const homeScreen = () => {
         // default:
         //   console.log('Exiting System')
         //   break
-//       }
-     })
- }
+      }
+    })
+}
 
-// const addDepartment = () => {
-//   inquirer.prompt([{
-//     type: 'input',
-//     name: 'name',
-//     message: 'Enter the name of the department:'
-//   }])
-//     .then(department => {
-//       db.query('INSERT INTO departments SET ?', department, err => {
-//         if (err) { console.log(err) }
-//       })
-//       console.log('Department added, returning to home screen...')
-//       homeScreen()
-//     })
-// }
+// Adds a department to the db
+const addDepartment = () => {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'name',
+    message: 'Enter the name of the department:'
+  }])
+    .then(department => {
+      db.query('INSERT INTO departments SET ?', department, err => {
+        if (err) { console.log(err) }
+      })
+      console.log('Department added, returning to home screen...')
+      homeScreen()
+    })
+}
 
-// const addRole = () => {
-//   inquirer.prompt([{
-//     type: 'input',
-//     name: 'title',
-//     message: 'Enter the title of the role:'
-//   },
-//   {
-//     type: 'input',
-//     name: 'salary',
-//     message: 'Enter the salary of the role:'
-//   },
-//   {
-//     type: 'input',
-//     name: 'department_id',
-//     message: 'Enter the id of the role:'
-//   }])
-//     .then(role => {
-//       db.query('INSERT INTO roles SET ?', role, err => {
-//         if (err) { console.log(err) }
-//       })
-//       console.log('Role added, returning to home screen...')
-//       homeScreen()
-//     })
-// }
+// Adds a role to the db
+const addRole = () => {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of the role:'
+  },
+  {
+    type: 'input',
+    name: 'salary',
+    message: 'Enter the salary of the role:'
+  },
+  {
+    type: 'input',
+    name: 'department_id',
+    message: 'Enter the id of the role:'
+  }])
+    .then(role => {
+      db.query('INSERT INTO roles SET ?', role, err => {
+        if (err) { console.log(err) }
+      })
+      console.log('Role added, returning to home screen...')
+      homeScreen()
+    })
+}
 
-// const addEmployee = () => {
-//   inquirer.prompt([{
-//     type: 'input',
-//     name: 'first_name',
-//     message: 'Enter the first name of the employee:'
-//   },
-//   {
-//     type: 'input',
-//     name: 'last_name',
-//     message: 'Enter the last name of the employee:'
-//   },
-//   {
-//     type: 'input',
-//     name: 'role_id',
-//     message: 'Enter the id of the employee:'
-//   },
-//   {
-//     type: 'list',
-//     name: 'managerBool',
-//     message: 'Is the employee a manager?',
-//     choices: ['Yes', 'No']
-//   }])
-//     .then(employee => {
-//       if (employee.managerBool === 'Yes') {
-//         delete employee.managerBool
-//         db.query('INSERT INTO employees SET ?', employee, err => {
-//           if (err) { console.log(err) }
-//         })
-//         homeScreen()
-//       } else if (employee.managerBool === 'No') {
-//         inquirer.prompt([{
-//           type: 'input',
-//           name: 'manager_id',
-//           message: 'Enter the id of employee\'s manager:'
-//         }])
-//           .then(nonManager => {
+//adds an employee to the db
+const addEmployee = () => {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'first_name',
+    message: 'Enter the first name of the employee:'
+  },
+  {
+    type: 'input',
+    name: 'last_name',
+    message: 'Enter the last name of the employee:'
+  },
+  {
+    type: 'input',
+    name: 'role_id',
+    message: 'Enter the id of the employee:'
+  },
+  {
+    type: 'list',
+    name: 'managerBool',
+    message: 'Is the employee a manager?',
+    choices: ['Yes', 'No']
+  }])
+    .then(manager => {
+      if (manager.managerBool === 'Yes') {
+        delete manager.managerBool
+        db.query('INSERT INTO employees SET ?', manager, err => {
+          if (err) { console.log(err) }
+        })
+        console.log('Manager added, returning to home screen...')
+        homeScreen()
 
-//             delete employee.managerBool
+      } else if (manager.managerBool === 'No') {
+        inquirer.prompt([{
+          type: 'input',
+          name: 'manager_id',
+          message: 'Enter the id of employee\'s manager:'
+        }])
+          .then(employee => {
 
-//             // let regularEmployee = {
-//             //   ...employee
-//             //   ...nonManager
-//             // }
+            delete manager.managerBool
 
-//             // db.query('INSERT INTO employees SET ?', regularEmployee, err => {
-//             //   if (err) { console.log(err) }
-//             // })
-//             homeScreen()
-//           })
-//       }
-//     })
-// }
+            let regEmployee = {
+              ...manager,
+              ...employee
+            }
 
-// const viewDepartments = () => {
+            db.query('INSERT INTO employees SET ?', regEmployee, err => {
+              if (err) { console.log(err) }
+            })
+            homeScreen()
+          })
+      }
+    })
+}
 
-// }
+const viewDepartments = () => {
+  db.query('SELECT * FROM departments', (err, departments) => {
+    console.table(departments)
+  })
+  homeScreen()
+}
 
 // const viewRoles = () => {
 
